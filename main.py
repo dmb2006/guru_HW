62428e0a90d1a932632f533f5d7fccc1455fedaa
Aleksandr Minin <dmb2006minin@yandex.ru>
2025-05-21 12:40:06 +0300

    delete url google

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from selene import browser, be, have
 
 browser.open('https://ya.ru')
-browser.open('https://google.com')
 browser.element('[name="text"]').should(be.blank).type('tutu').press_enter()
 browser.element('html').should(have.text('Билеты и отели онлайн на официальном сайте'))
 
